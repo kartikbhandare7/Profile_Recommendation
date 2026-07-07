f5a04992297ce7ae95558f356a7717854858b444
Kartik Bhandare <kartikbhandare57@gmail.com>
2026-07-07 19:46:24 +0530

    chore: initialize Node.js project with Express and PostgreSQL setup

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const pool = require("./config/db");
+
+const app = express();
+
+app.use(express.json());
+
+pool.query("SELECT NOW()")
+    .then(result => {
+        console.log("Database Connected");
+        console.log(result.rows[0]);
+    })
+    .catch(err => {
+        console.log(err);
+    });
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
